3b2bb4c6c40621724a388f59c7358adf661ef151
James Kelleher <thisisjameskelleher@gmail.com>
2018-09-29 14:12:19 +0100

    Removed sample code

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,35 +4,3 @@ module Lib
 
 
 someFunc :: IO ()
-someFunc = do
-  putStrLn "This is my program. There are many like it but this one is mine."
-  putStrLn ( "10 / 2 = " ++ show (divideSafe 10 2)) -- divideSafe(10, 2)
-  putStrLn $ "10 / 0 = " ++ show (divideSafe 10 0)
-  putStrLn $ "10 / 0 = " ++ show (divideSafe 10 4)
-
-
--- my first attempt - nasty errors await
--- divideSafe :: (Show a, Eq a, Fractional a) => a -> a -> a
--- divideSafe x y = x / y
-
--- my second attempt - adding Maybe a to support error return, but...
--- divideSafe :: (Show a, Eq a, Fractional a) => a -> a -> Maybe a
--- divideSafe x y = Just $ x / y
-
-
--- changing result to Maybe a, but something wrong?
--- divideSafe :: (Eq a, Fractional a) => a -> a -> Maybe a
--- divideSafe x y = Just $ x / y
--- divideSafe _ 0 = Nothing
-
--- getting there
--- divideSafe :: (Eq a, Fractional a) => a -> a -> Maybe a
--- divideSafe _ 0 = Nothing
--- divideSafe x y = Just $ x / y
-
--- elaboration
--- TODO: i need to fix this seriously.
-divideSafe :: (Ord a, Eq a, Fractional a) => a -> a -> Maybe a
-divideSafe _ 0 = Nothing
-divideSafe x y = if x > 100 then Nothing else Just $ x / y
-
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -6,21 +6,3 @@ import           Test.QuickCheck
 
 spec :: Spec
 spec = do
-  describe "Divide Function" $ do
-
-    it "returns Just 0 for 0/x" $ do
-      (divideSafe 0 3) == Just 0  `shouldBe` True
-
-    it "returns Just 5 for 10/2" $ do
-      (divideSafe 10 2) `shouldBe` Just 5
-
-    it "returns Nothing for x/0" $ do
-      (divideSafe 10 0) `shouldBe` Nothing
-
-    -- describe "should return nothing for x > 100" $ do
-    it "should return nothing for x > 100, y == 0" $ do
-      (divideSafe 101 0) `shouldBe` Nothing
-
-    it "should return nothing for x > 100, y == 10" $ do
-      (divideSafe 101 10) `shouldBe` Nothing
-
