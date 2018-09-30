f150a4c6e73b8fc49edaeada5cbcd84e0cb9437c
James Kelleher <thisisjameskelleher@gmail.com>
2018-09-30 10:03:44 +0100

    BST datatype and four sample BSTs: - bstEmpty - no nodes - bstRoot - one root node - bstChar - small Char BST - bstSample - larger Int BST

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,40 @@
 module Lib
-    ( someFunc 
+    ( bstContains, bstLCA, BST, bstEmpty, bstRoot, bstChar, bstSample
     ) where
 
 
-someFunc :: IO ()
+data BST a = Nil | Node a (BST a) (BST a) deriving Show
+
+bstEmpty :: BST Int
+bstEmpty = Nil
+
+bstRoot :: BST Int
+bstRoot = Node 10 Nil Nil
+
+bstChar :: BST Char
+bstChar = Node 'm'
+    (Node 'c'
+        (Node 'a' Nil Nil)
+        Nil)
+    (Node 'q' Nil
+        (Node 't' Nil Nil))
+
+bstSample :: BST Int
+bstSample = Node 7
+    (Node 2
+        (Node 1 Nil Nil)
+        (Node 5
+            (Node 4
+                (Node 3 Nil Nil)
+                Nil)
+            (Node 6 Nil Nil)))
+    (Node 12
+        (Node 9
+            (Node 8 Nil Nil)
+            (Node 11
+                (Node 10 Nil Nil)
+                Nil))
+        (Node 13 Nil
+            (Node 15
+                (Node 14 Nil Nil)
+                (Node 16 Nil Nil))))
